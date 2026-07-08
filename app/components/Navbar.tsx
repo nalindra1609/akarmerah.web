1a789030aa20e7664211411c9399586ac992a2f1
nalindra1609 <nalindra1609@gmail.com>
2026-07-08 18:53:06 +0700

    merakit web akar merah

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-3">
-            <img src="/logo.png" alt="Logo Akar Merah" className="h-10 w-auto object-contain" />
+            <img src="/logo.PNG" alt="Logo Akar Merah" className="h-10 w-auto object-contain" />
             <span className="font-black tracking-widest text-lg">AKAR MERAH</span>
           </div>
 
